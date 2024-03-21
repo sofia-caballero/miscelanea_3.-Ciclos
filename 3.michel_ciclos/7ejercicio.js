a653be2d098d93b33378069fea3f35a9b693fafc
sofia-caballero <160689113+sofia-caballero@users.noreply.github.com>
2024-03-20 22:47:04 -0500

    Update 7ejercicio.js

--- 3.michel_ciclos/7ejercicio.js
+++ 3.michel_ciclos/7ejercicio.js
@@ -2,19 +2,21 @@ console.log("7. Sumar todos los números que se digitan por teclado mientras no 
 
 console.log("ingrese los numero");
 
-
-
-
 let num1 = 3;
 let num2 = 7;
 let num3 = 2;
 let num4 = 5;
 let num5 = 9;
 
- suma=num1+num2+num3+num4+num5;
+suma=num1+num2+num3+num4+num5;
+
+if (suma<=0) {
+    console.log("digite otros numero que no sean cero.");
+}
 
 while (suma > 0) {
 console.log("la suma es:",suma);
-suma;
+suma=0;
+
 }
 
